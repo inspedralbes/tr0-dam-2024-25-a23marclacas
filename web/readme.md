298adaca9eab2992bb6d8ea525c6b37b2d697f6e
Martí <a23marclacas@inspedralbes.cat>
2024-09-27 10:45:00 +0200

    27/09/2024

--- web/readme.md
+++ web/readme.md
@@ -1,10 +1,24 @@
-# Carpeta _/web_
+# web
 
-Aquesta carpeta ha de contenir l'aplicació, tot el que s'hagi de posar a la carpeta "public_html" del site.
-Típicament, els següents fitxers organitzats en les carpetes corresponents:
- -  html
- -  php
- -  png,jpg ...
- -  js
- -  css
- -  
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ web/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<template>
+  <html>
+    <head>
+        <title>Adminisrador del Qüestionari</title>
+        <link rel="stylesheet" href="./estils.css">
+    </head>
+    <body>
+        <div>
+            <h1 class="titol">Administrador del Qüestionari</h1>
+        </div>
+        <div class="graella">
+            <div class="item1"><h2>Pregunta</h2></div>
+            <div class="item2"><img width="200" src="./Imatges/Pregunta 1.jpg"></div>
+            <div class="item3"><p>resposta1</p></div>
+            <div class="item4"><p>resposta2</p></div>
+            <div class="item5"><p>resposta3</p></div>
+            <div class="item6"><p>resposta4</p></div>
+            <div class="item7"><p>resposta correcte</p></div>
+            <div class="boto1"><button>Update</button></div>
+            <div class="boto2"><button>Delete</button></div>
+        </div>
+    </body>
+</html>
+</template>
+
